fbdc49e97956d0e230dab7753af28dec44dde740
Dr Julius Bright <juliasbright@gmail.com>
2023-09-25 11:29:46 +0200

    modified:   .gitignore 	new file:   wallet-frontend/.gitignore 	new file:   wallet-frontend/README.md 	new file:   wallet-frontend/babel.config.js 	new file:   wallet-frontend/jsconfig.json 	new file:   wallet-frontend/package.json 	new file:   wallet-frontend/public/favicon.ico 	new file:   wallet-frontend/public/index.html 	new file:   wallet-frontend/src/App.vue 	new file:   wallet-frontend/src/assets/logo.png 	new file:   wallet-frontend/src/components/EndpointList.vue 	new file:   wallet-frontend/src/main.js 	new file:   wallet-frontend/vue.config.js 	new file:   wallet-frontend/yarn.lock

---
+++ wallet-frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <EndpointList/>
+</template>
+
+<script>
+import EndpointList from './components/EndpointList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EndpointList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ wallet-frontend/src/components/EndpointList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h1>API Endpoints</h1>
+      <ul>
+        <li v-for="endpoint in endpoints" :key="endpoint.endpoint">
+          <h2>{{ endpoint.endpoint }}</h2>
+          <p>Methods: {{ endpoint.methods }}</p>
+          <p>Path: {{ endpoint.path }}</p>
+          <p>Example:</p>
+          <pre>{{ endpoint.example }}</pre>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  /* eslint-disable */
+  import axios from 'axios'; // Import Axios
+  
+  export default {
+    data() {
+      return {
+        endpoints: [],
+      };
+    },
+    created() {
+      // Make an Axios GET request to your Flask app to fetch the list of endpoints and examples
+      axios.get('http://127.0.0.1:5000').then(response => {
+        this.endpoints = response.data.routes;
+        console.log(response);
+      }).catch(error => {
+        console.error('Error fetching endpoints:', error);
+      });
+    },
+  };
+  </script>
+  
